8ca09a071f6d9ba01ccb78645f0e6b2f587f8918
ReyzerMC <reyzervt@gmail.com>
2026-02-16 22:38:53 +0100

    Docummented config.py & utils.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 import uuid
 
-VERSION = "1.0.0"
-_KEY_ID = 0
-CONFIG_DIR = os.path.expanduser("~/.config/cipher")
-KEY_PATH = os.path.join(CONFIG_DIR, "key")
-KEY_DIR = os.path.join(KEY_PATH, f"{_KEY_ID}.key")
-SENSITIVE_DIRS = [
+VERSION = "1.0.0" # Actual cipher version
+_KEY_ID = 0 # Key ID initialization
+CONFIG_DIR = os.path.expanduser("~/.config/cipher") # Config DIR
+KEY_PATH = os.path.join(CONFIG_DIR, "key") # Key path in the config DIR
+KEY_DIR = os.path.join(KEY_PATH, f"{_KEY_ID}.key") # Key DIR in the config
+SENSITIVE_DIRS = [ # Sensitive dirs to avoid accidentally ecnrypting something sensitive like config dirs and user files (.local, .config)
     os.path.expanduser("~/.config"), os.path.expanduser("~/.var"),
     os.path.expanduser("~/.cache"), os.path.expanduser("~/.local"),
     os.path.expanduser("~/.firefox"), os.path.expanduser("~/.mozilla"),
@@ -15,17 +15,17 @@ SENSITIVE_DIRS = [
     os.path.expanduser("~/.python"), os.path.expanduser("~/.thunderbird")
 ]
 
-def dirs():
-    global _KEY_ID, KEY_DIR
-    if not os.path.exists(CONFIG_DIR):
+def dirs(): # Main function to create config dirs (Key dir) and generates the key path with generateID() function
+    global _KEY_ID, KEY_DIR # Globalizing variables
+    if not os.path.exists(CONFIG_DIR): # If not exists check
         os.makedirs(CONFIG_DIR, exist_ok=True)
 
-    if not os.path.exists(KEY_PATH):
+    if not os.path.exists(KEY_PATH): # Same check but for key path
         os.makedirs(KEY_PATH, exist_ok=True)
 
-    if not os.path.exists(KEY_DIR):
-        _KEY_ID = generateID()
+    if not os.path.exists(KEY_DIR): # Same check but for generate the key
+        _KEY_ID = generateID() # ID generation
         KEY_DIR = os.path.join(KEY_PATH, f"{_KEY_ID}.key")
 
-def generateID():
+def generateID(): # Function to generate de ID
     return uuid.uuid4().int
--- utils.py
+++ utils.py
@@ -4,35 +4,35 @@ import nacl.utils
 from nacl.secret import SecretBox
 from config import SENSITIVE_DIRS, KEY_DIR
 
-def antiSily():
-    r = input(f"!! Are you sure you want to perform this action for this entry? [y/N]: ")
+def antiSily(): # Function to avoid accidentally encrypt something
+    r = input(f"!! Are you sure you want to perform this action for this entry? [y/N]: ") # Mini check to avoid it
     if r.lower() == "y":
-        pass
+        pass # Continue in case of Y
     else:
-        print("Operation cancelled. Exiting.", file=sys.stderr)
+        print("Operation cancelled. Exiting.", file=sys.stderr) # Error with sys.stderr if you select N
         sys.exit(1)
 
-def printTree(path, prefix=""):
+def printTree(path, prefix=""): # Auxiliar function to print the tree of what are you encrypting
     print(prefix + os.path.basename(path) + "/")
-    if os.path.isdir(path):
-        for item in os.listdir(path):
+    if os.path.isdir(path): # Verification if is a dir
+        for item in os.listdir(path): # List dirs
             full_path = os.path.join(path, item)
-            if os.path.isdir(full_path):
+            if os.path.isdir(full_path): # Build the tree
                 printTree(full_path, prefix + "│   ")
             else:
                 print(prefix + "│   " + item)
 
-def isSensitive(path):
-    abs_path = os.path.abspath(path)
-    for s in SENSITIVE_DIRS:
+def isSensitive(path): # Funtion to chech if a path contains sensitive dirs
+    abs_path = os.path.abspath(path) # Get the absolute path
+    for s in SENSITIVE_DIRS: # Check every dir in the path, if contains a sensitive dir
         if abs_path.startswith(os.path.abspath(s)):
-            warn = input(f"!! Sensitive directory detected: {abs_path}\nAre you sure? [y/N]: ")
+            warn = input(f"!! Sensitive directory detected: {abs_path}\nAre you sure? [y/N]: ") # Question if you know what are you doing
             if warn.lower() != "y":
                 print("Operation cancelled. Exiting.", file=sys.stderr)
                 sys.exit(1)
-            break  # solo preguntar una vez
+            break # This is serious, you will only be asked once.
 
-def generateKey():
+def generateKey(): # Generates the key that is stored at ~/.config/cipher/key/'ID'.key
     key = nacl.utils.random(SecretBox.KEY_SIZE)
     with open(KEY_DIR, "wb") as keyFile:
         keyFile.write(key)
